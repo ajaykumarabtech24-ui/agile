057c0fd4de7db1be939a66a4306a1fdb2d751bb2
ajay <ajaykumarabtech24@rvu.edu.in>
2026-02-27 11:40:20 +0530

    commit

---
+++ agile.py
@@ -0,0 +1,23 @@
+# Import libraries
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# Create data
+X = np.array([1, 2, 3, 4]).reshape(-1, 1)   # Independent variable
+y = np.array([2, 4, 6, 8])                 # Dependent variable
+
+# Create model
+model = LinearRegression()
+
+# Train model
+model.fit(X, y)
+
+# Get slope and intercept
+print("Intercept (a):", model.intercept_)
+print("Slope (b):", model.coef_[0])
+
+# Make prediction
+new_value = np.array([[5]])
+prediction = model.predict(new_value)
+
+print("Prediction for x = 5:", prediction[0])
