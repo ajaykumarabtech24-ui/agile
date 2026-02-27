742eb2f081a9ce948f45111f0a6ed002c81089f8
ajaykumarabtech24-ui <ajaykumarabtech24@rvu.edu.in>
2026-02-27 16:03:59 +0530

    Update agile.py

--- agile.py
+++ agile.py
@@ -3,8 +3,8 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 # Create data
-X = np.array([1, 2, 3, 4]).reshape(-1, 1)   # Independent variable
-y = np.array([2, 4, 6, 8])                 # Dependent variable
+X = np.array([1, 2, 3, 4,5]).reshape(-1, 1)   # Independent variable
+y = np.array([2, 4, 6, 8,10])                 # Dependent variable
 
 # Create model
 model = LinearRegression()
